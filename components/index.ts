de49af00b0aa55e9b21e3c33ce52675e2033d1f4
godfrey <joyyujiepeng@gmail.com>
2023-07-14 22:08:30 +0800

    set up the project

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+import CustomButton from "./CustomButton";
+
+export {
+    Hero,
+    Footer,
+    NavBar,
+    CustomButton
+}
