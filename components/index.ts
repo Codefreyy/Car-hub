c7a33ca0bed68aeffec6f131c2be987ae7249367
godfrey <joyyujiepeng@gmail.com>
2023-07-16 13:38:43 +0800

    feat: add the searchManufacturers bar

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,16 @@ import Hero from "./Hero";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
 
 export {
     Hero,
     Footer,
     NavBar,
-    CustomButton
+    CustomButton,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer
 }
